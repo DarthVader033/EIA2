2395a3d12cd1a95475b451da36c6d83803c1a74c
DarthVader033 <alitamaier@gmail.com>
2024-04-26 21:49:21 +0200

    aufgabe

---
+++ L03/liste.js
@@ -0,0 +1,17 @@
+const einkaufenData = [
+    {
+        menge: 1,
+        artikel: "Apfel",
+        kommentar: "Rot",
+        gekauft: false
+    },
+    {
+        menge: 2,
+        artikel: "Milch",
+        kommentar: "Hafer",
+        gekauft: false
+    },
+    
+];
+
+export default einkaufenData;
--- L03/liste.ts
+++ L03/liste.ts
@@ -1,7 +1,14 @@
+import einkaufenData from './liste.js';
+
+
+console.log(einkaufenData);
+
+
+
 /*
 Aufgabe: <Einkaufsliste>
 Name: <Alita Maier>
 Matrikel: <275106>
-Datum: <20.04.24>
+Datum: <26.04.24>
 Quellen: <Zusammenarbeit mit Franciska Egri>
 */
